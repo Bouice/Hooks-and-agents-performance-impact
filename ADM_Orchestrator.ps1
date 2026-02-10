42a8fc64299e48765a3aa15738a26b21eaf98830
Brice DUPONT <brice.dupont@gmail.com>
2026-02-10 17:14:33 +0100

    Update ADM_Orchestrator.ps1

--- ADM_Orchestrator.ps1
+++ ADM_Orchestrator.ps1
@@ -1,43 +1,47 @@
 # ==============================================================================
 # ORCHESTRATEUR ADMIN - GESTION DES SERVICES IVANTI
 # ==============================================================================
-$SignalFile = Join-Path $env:TEMP "bench_signal.txt"
-$Svc = @{ AC="AmAgent"; UWM="EmCoreService"; FD="FileDirectorService" }
+$SignalFile = "C:/Applications/bench_signal.txt"
+$Svc = @{ AppControl="AppSense Application Manager Agent"; UWM="AppSense EmCoreService"; FD="DataNow_Service" }
 
 function Set-AllOn {
-    Write-Host "[+] Restauration : Tout à ON" -ForegroundColor Gray
+    Write-Host "[+] Restauration : Tout a ON" -ForegroundColor Blue
     foreach($s in $Svc.Values) { 
         Set-Service $s -StartupType Automatic -ErrorAction SilentlyContinue
         Start-Service $s -ErrorAction SilentlyContinue 
     }
 }
 
-# Séquence : Ce qu'on veut ÉTEINDRE pour chaque Run
+# Sequence : Ce qu'on veut eteindre pour chaque Run
 $Sequence = @(
-    @{ Name="RUN1_FULL_STACK"; OFF=@() },
-    @{ Name="RUN2_NO_APPCO";   OFF=@($Svc.AC) },
-    @{ Name="RUN3_NO_UWM";     OFF=@($Svc.UWM) },
-    @{ Name="RUN4_NO_FD";      OFF=@($Svc.FD) },
-    @{ Name="RUN5_UWM_OFF_AC_ON"; OFF=@($Svc.UWM) }
+    @{ Name="Benchmark 1 : Tous services actifs"; OFF=@() },
+    @{ Name="Benchmark 2 : Application Control Desactive";   OFF=@($Svc.AppControl) },
+    @{ Name="Benchmark 3 : UWM Desactive";     OFF=@($Svc.UWM) },
+    @{ Name="Benchmark 4 : File Director Desactive";      OFF=@($Svc.FD) },
+    @{ Name="Benchmark 5 : UWM Reactive"; ON=@($Svc.UWM) },
+	@{ Name="Benchmark 6 : UWM + AppControl Reactive"; ON=@($Svc.AppControl) }
 )
 
 try {
-    Set-AllOn
     foreach ($Step in $Sequence) {
-        Write-Host "`n>>> Préparation : $($Step.Name)" -ForegroundColor Yellow
+        Write-Host "`n---> Preparation : $($Step.Name)" -ForegroundColor Yellow
         
-        # 1. État des lieux : Tout ON puis on coupe la cible
-        Set-AllOn
+        # 1. etat des lieux : Tout ON puis on coupe la cible
         foreach ($target in $Step.OFF) { 
             Stop-Service $target -Force -ErrorAction SilentlyContinue
             Set-Service $target -StartupType Disabled -ErrorAction SilentlyContinue
-            Write-Host "[-] $target mis à OFF" -ForegroundColor Red
+            Write-Host "[-] $target mis a OFF" -ForegroundColor Red
+        }
+		foreach ($target in $Step.ON) { 
+            Set-Service $target -StartupType Automatic -ErrorAction SilentlyContinue
+			Start-Service $target
+            Write-Host "[+] $target mis a ON" -ForegroundColor Blue
         }
 
         # 2. On attend la stabilisation des drivers (Fltmc)
-        Start-Sleep -Seconds 5
+        Start-Sleep -Seconds 20
         
-        # 3. On crée le signal pour le script User
+        # 3. On cree le signal pour le script User
         "READY" | Out-File $SignalFile
 
         # 4. On attend que l'User finisse (il supprimera le fichier)
@@ -47,5 +51,5 @@ try {
 } finally {
     Set-AllOn
     if (Test-Path $SignalFile) { Remove-Item $SignalFile }
-    Write-Host "`n[FIN] Séquence terminée. Système restauré." -ForegroundColor Magenta
+    Write-Host "`n[FIN] Sequence terminee. Systeme restaure." -ForegroundColor Magenta
 }
